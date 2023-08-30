4e0af38ceb1cf787e21c7088cf9ac90c89d4bfd3
Rajnish <b421037@iiit-bh.ac.in>
2023-08-30 12:12:21 +0530

    Footer and navbar styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,10 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <div
-        className="text-white pb-3 pt-3 text-align-center text-center fixed-bottom"
-        style={{ background: "linear-gradient(to left, #4b6cb7, #182848)" }}
-      >
+      <div className="text-white pb-3 pt-3 text-align-center text-center fixed-bottom footer-design">
         <b>Copyright &copy; MLSA Chapter - IIIT Bhubaneswar</b>
       </div>
     </div>
