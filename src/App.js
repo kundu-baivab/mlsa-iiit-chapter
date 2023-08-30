f93e02a3936602be521390df7436396744f5ab87
Rajnish <b421037@iiit-bh.ac.in>
2023-08-30 10:50:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Navbar />
       <BrowserRouter>
         <Routes>
