b76d3e5dd96916e1d921908d42249ea4cd5ac538
Rajnish <b421037@iiit-bh.ac.in>
2023-09-03 14:04:55 +0530

    animate library added

--- src/pages/Milestones.js
+++ src/pages/Milestones.js
@@ -1,71 +1,87 @@
 import React from "react";
-
-import './Milestones.css'
-import 'animate.css'
-
+import "../CSS_files/Milestones.css";
+import "animate.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 function Milestones() {
-
-
   return (
-
     <>
       <div className="parent-container ">
-
         <div className="container1">
           <div className="child1">
             <div className="new-image-container animate__animated animate__slideInLeft ">
-                  <img className="new-image" src="LevelNew.png" alt="MLSA New Logo"/>  
+              <img
+                className="new-image"
+                src="./assets/LevelNew.png"
+                alt="MLSA New Logo"
+              />
             </div>
             <div className="intro-container hidden popup">
-                <h2>MLSA MILESTONES</h2>
-                <p>
-                  Student Ambassadors are required to complete various pre-identified activities in order to grow their skills on a well-defined road to success. As you complete these activities, your program milestones are advanced. Additional program benefits are unlocked too. 
-                  Milestones are advanced in order from New to Alpha, Alpha to Beta and Beta to Gold. Program certificates are updated with each milestone advancement.
-                  Milestones showcase your expertise and let the world know about your achievements. You are encouraged to share your milestone advancements with your local and online communities.
-                </p>
+              <h2>MLSA MILESTONES</h2>
+              <p>
+                Student Ambassadors are required to complete various
+                pre-identified activities in order to grow their skills on a
+                well-defined road to success. As you complete these activities,
+                your program milestones are advanced. Additional program
+                benefits are unlocked too. Milestones are advanced in order from
+                New to Alpha, Alpha to Beta and Beta to Gold. Program
+                certificates are updated with each milestone advancement.
+                Milestones showcase your expertise and let the world know about
+                your achievements. You are encouraged to share your milestone
+                advancements with your local and online communities.
+              </p>
             </div>
           </div>
-         
         </div>
 
-            <div className="container2">
-              <div className="child2">
-              <div className="alpha-image-container">
-                <img className="alpha-image" src="LevelAlpha.png" alt="MLSA Alpha Logo"/>  
-              </div>
-
-              <div className="alpha-container popup">
-                <h3>ALPHA MLSA</h3>
-                <div className="alpha-child">
-                  <p>Student Ambassadors that complete a learning path on Microsoft Learn will earn the Alpha badge.</p>
-                </div>
-                <div className="alpha-perks">
-                  <ul>
-                    <li>USD150 monthly Azure credits</li>
-                    <li>Visual Studio Enterprise subscription</li>
-                    <li>StudentAmbassadors.com email address</li>
-                    <li>Event workshop kits</li>
-                    <li>Incentives for event attendees</li>
-                  </ul>
-
-                </div>
-                </div>
-              </div>
-            
+        <div className="container2">
+          <div className="child2">
+            <div className="alpha-image-container">
+              <img
+                className="alpha-image"
+                src="./assets/LevelAlpha.png"
+                alt="MLSA Alpha Logo"
+              />
             </div>
 
-            <div className="container3">
-              <div className="child3">
-              <div className="beta-image-container">
-                <img className="beta-image" src="LevelBeta.png" alt="MLSA Beta Logo"/>  
-              </div>  
+            <div className="alpha-container popup">
+              <h3>ALPHA MLSA</h3>
+              <div className="alpha-child">
+                <p>
+                  Student Ambassadors that complete a learning path on Microsoft
+                  Learn will earn the Alpha badge.
+                </p>
+              </div>
+              <div className="alpha-perks">
+                <ul>
+                  <li>USD150 monthly Azure credits</li>
+                  <li>Visual Studio Enterprise subscription</li>
+                  <li>StudentAmbassadors.com email address</li>
+                  <li>Event workshop kits</li>
+                  <li>Incentives for event attendees</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="container3">
+          <div className="child3">
+            <div className="beta-image-container">
+              <img
+                className="beta-image"
+                src="./assets/LevelBeta.png"
+                alt="MLSA Beta Logo"
+              />
+            </div>
             <div className="beta-container popup">
-                <h3>BETA MLSA</h3>
+              <h3>BETA MLSA</h3>
               <div className="beta-child">
-                <p>Student Ambassadors who plan and host an event for other students in their community will earn the Beta badge.</p>
+                <p>
+                  Student Ambassadors who plan and host an event for other
+                  students in their community will earn the Beta badge.
+                </p>
               </div>
               <div className="beta-perks">
                 <ul>
@@ -76,22 +92,26 @@ function Milestones() {
                   <li>Mentorship</li>
                 </ul>
               </div>
-              </div>
-           
-              </div>
-
-
             </div>
+          </div>
+        </div>
 
-            <div className="container4">
-              <div className="child4">
-              <div className="gold-image-container">
-                <img className="gold-image" src="LevelGold.png" alt="MLSA Gold Logo"/>  
-              </div>
+        <div className="container4">
+          <div className="child4">
+            <div className="gold-image-container">
+              <img
+                className="gold-image"
+                src="./assets/LevelGold.png"
+                alt="MLSA Gold Logo"
+              />
+            </div>
             <div className="gold-container popup">
-                <h3>GOLD MLSA</h3>
+              <h3>GOLD MLSA</h3>
               <div className="gold-child">
-                <p>Student Ambassadors who have gone above and beyond serving their local community will be invited to the Gold milestone.</p>
+                <p>
+                  Student Ambassadors who have gone above and beyond serving
+                  their local community will be invited to the Gold milestone.
+                </p>
               </div>
               <div className="gold-perks">
                 <ul>
@@ -102,28 +122,18 @@ function Milestones() {
                   <li>Exclusive Gold swag</li>
                 </ul>
               </div>
-              </div>
-              </div>
-             
-
-          </div>
-
             </div>
+          </div>
+        </div>
+      </div>
 
+      <div>
+        <Navbar />
+        <h1>Milestones</h1>
+        <Footer />
+      </div>
     </>
-
-    <div>
-      <Navbar />
-      <h1>Milestones</h1>
-      <Footer />
-    </div>
-
   );
 }
 
 export default Milestones;
-
-
-
-
-
