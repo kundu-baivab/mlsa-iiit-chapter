0c5d51f4a00c192f1632eb8aeabe467b8035e58c
Rajnish <b421037@iiit-bh.ac.in>
2023-09-18 10:02:41 +0530

    milestones page restructured

--- src/pages/Milestones.js
+++ src/pages/Milestones.js
@@ -9,29 +9,47 @@ function Milestones() {
     <>
       <Navbar />
       <div className="parent-container container">
-        <div className="container1">
-          <div className="child1">
-            <div className="new-image-container animate__animated animate__slideInLeft ">
+        <div className="container2">
+          <div className="child2">
+            <div className="alpha-image-container">
               <img
-                className="new-image"
-                src="./assets/LevelNew.png"
-                alt="MLSA New Logo"
+                className="alpha-image"
+                src="./assets/LevelAlpha.png"
+                alt="MLSA Alpha Logo"
               />
             </div>
-            <div className="intro-container hidden popup">
-              <h2>MLSA MILESTONES</h2>
-              <p>
-                Student Ambassadors are required to complete various
-                pre-identified activities in order to grow their skills on a
-                well-defined road to success. As you complete these activities,
-                your program milestones are advanced. Additional program
-                benefits are unlocked too. Milestones are advanced in order from
-                New to Alpha, Alpha to Beta and Beta to Gold. Program
-                certificates are updated with each milestone advancement.
-                Milestones showcase your expertise and let the world know about
-                your achievements. You are encouraged to share your milestone
-                advancements with your local and online communities.
-              </p>
+
+            <div className="alpha-container popup">
+              <h3>MLSA MILESTONES</h3>
+              <div className="alpha-child">
+                <p>
+                  Student Ambassadors are required to complete various
+                  pre-identified activities in order to grow their skills on a
+                  well-defined road to success. As you complete these
+                  activities, your program milestones are advanced. Additional
+                  program benefits are unlocked too. Milestones are advanced in
+                  order from{" "}
+                  <strong style={{ color: "#4b6cb7" }}>
+                    New to Alpha, Alpha to Beta
+                  </strong>{" "}
+                  and <strong style={{ color: "#4b6cb7" }}>Beta to Gold</strong>{" "}
+                  <strong style={{ color: "#4b6cb7" }}>
+                    .
+                    <br />
+                    <br />
+                    Program certificates
+                  </strong>{" "}
+                  are updated with each milestone advancement. Milestones
+                  showcase your expertise and let the world know about your
+                  achievements. You are encouraged to share your milestone
+                  advancements with your{" "}
+                  <strong style={{ color: "#4b6cb7" }}>local</strong> and{" "}
+                  <strong style={{ color: "#4b6cb7" }}>
+                    online communities
+                  </strong>
+                  .
+                </p>
+              </div>
             </div>
           </div>
         </div>
@@ -56,11 +74,31 @@ function Milestones() {
               </div>
               <div className="alpha-perks">
                 <ul>
-                  <li>USD150 monthly Azure credits</li>
-                  <li>Visual Studio Enterprise subscription</li>
-                  <li>StudentAmbassadors.com email address</li>
-                  <li>Event workshop kits</li>
-                  <li>Incentives for event attendees</li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>
+                      150$ monthly Azure credits
+                    </strong>
+                  </li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>
+                      Visual Studio Enterprise subscription
+                    </strong>
+                  </li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>
+                      StudentAmbassadors.com email address
+                    </strong>
+                  </li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>
+                      Event workshop kits
+                    </strong>
+                  </li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>
+                      Incentives for event attendees
+                    </strong>
+                  </li>
                 </ul>
               </div>
             </div>
@@ -86,11 +124,18 @@ function Milestones() {
               </div>
               <div className="beta-perks">
                 <ul>
-                  <li>Alpha benefits</li>
-                  <li>Swag</li>
-                  <li>StudentAmbassadors.com email address</li>
-                  <li>Event support</li>
-                  <li>Mentorship</li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>Alpha benefits</strong>
+                  </li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>Swag</strong>
+                  </li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>Event support</strong>
+                  </li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>Mentorship</strong>
+                  </li>
                 </ul>
               </div>
             </div>
@@ -116,11 +161,31 @@ function Milestones() {
               </div>
               <div className="gold-perks">
                 <ul>
-                  <li>Alpha and Beta benefits</li>
-                  <li>Program leadership opportunities</li>
-                  <li>Microsoft Most Valuable Professional (MVP) mentorship</li>
-                  <li>May be invited to the MVP program</li>
-                  <li>Exclusive Gold swag</li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>
+                      Alpha and Beta benefits
+                    </strong>
+                  </li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>
+                      Program leadership opportunities
+                    </strong>
+                  </li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>
+                      Microsoft Most Valuable Professional (MVP) mentorship
+                    </strong>
+                  </li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>
+                      May be invited to the MVP program
+                    </strong>
+                  </li>
+                  <li>
+                    <strong style={{ color: "#4b6cb7" }}>
+                      Exclusive Gold swag
+                    </strong>
+                  </li>
                 </ul>
               </div>
             </div>
@@ -130,7 +195,6 @@ function Milestones() {
 
       <div>
         <Navbar />
-        <h1>Milestones</h1>
         <Footer />
       </div>
     </>
